b53f1b5ea46730cae0e78821bb1ac78d5961e591
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2020-10-23 21:15:16 -0700

    h

--- leavejoinbot.py
+++ leavejoinbot.py
@@ -4,7 +4,8 @@ import discord
 intents=discord.Intents.all()
 client=discord.Client(intents=intents)
 
-s=input("What is the text channel ID you want to put it in?")
+s=input("What is the text channel ID you want to put it in?: ")
+st=input("What is the token of the bot?: ")
 
 
 @client.event
@@ -22,13 +23,10 @@ async def on_message(message):
         joke=pyjokes.get_joke(language='en',category='neutral')
         await message.channel.send(joke)
 
-    if(message.content.startswith('lol')): 
-        joke=pyjokes.get_joke(language='en',category='neutral')
-        await message.channel.send(joke)
 @client.event
 async def on_member_join(member):
 
     await welcome_channel.send("Welcome "+member.display_name)
 
 
-client.run('wow cool token')
+client.run(str(st))
